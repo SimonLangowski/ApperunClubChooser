0532e66d29a444d4b7b1c44c0124615b26e9e08f
pizzamath <slangows@purdue.edu>
2017-06-26 17:02:59 -0400

    Added name entry button

--- app/src/main/java/com/apperun/clubchooser/TitleActivity.java
+++ app/src/main/java/com/apperun/clubchooser/TitleActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -14,6 +15,7 @@ public class TitleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_title);
         Button start = (Button) findViewById(R.id.start);
         Button restart = (Button) findViewById(R.id.restart);
+        EditText nameEntry = (EditText) findViewById(R.id.nameEntry);
         start.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
                 Intent intent = new Intent(getApplicationContext(), QuestionActivity.class);
