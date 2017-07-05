8a8e25cc6744256071627f521a3821926978357f
pizzamath <slangows@purdue.edu>
2017-07-05 11:17:58 -0400

    Stores name

--- app/src/main/java/com/apperun/clubchooser/TitleActivity.java
+++ app/src/main/java/com/apperun/clubchooser/TitleActivity.java
@@ -1,11 +1,17 @@
 package com.apperun.clubchooser;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -13,13 +19,35 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.titleLayout);
+        layout.setMinimumWidth(getWindowManager().getDefaultDisplay().getWidth());
         Button start = (Button) findViewById(R.id.start);
         Button results= (Button) findViewById(R.id.restart); //no longer restarts
-        EditText nameEntry = (EditText) findViewById(R.id.nameEntry);
+        TextView name = (TextView) findViewById(R.id.name);
+        final EditText nameEntry = (EditText) findViewById(R.id.nameEntry);
+
+        final SharedPreferences storage;
+        storage = getPreferences(0);
+        boolean k = false;
+        if (storage.contains("name")){
+            nameEntry.setVisibility(View.INVISIBLE);
+            String welcome = "Welcome back " + storage.getString("name", "!") + "!";
+            name.setText(welcome);
+        } else {
+            name.setVisibility(View.INVISIBLE);
+            k = true;
+        }
+        final boolean keepName = k;
+
         start.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
                 Intent intent = new Intent(getApplicationContext(), QuestionActivity.class);
                 intent.putExtra("restart", true);
+                if (keepName){
+                    SharedPreferences.Editor editor = storage.edit();
+                    editor.putString("name", nameEntry.getText().toString());
+                    editor.commit();
+                }
                 startActivity(intent);
             }
         });
@@ -28,6 +56,11 @@ public class TitleActivity extends AppCompatActivity {
             public void onClick(View v){
                 Intent intent = new Intent(getApplicationContext(), QuestionActivity.class);
                 intent.putExtra("restart", false);
+                if (keepName){
+                    SharedPreferences.Editor editor = storage.edit();
+                    editor.putString("name", nameEntry.getText().toString());
+                    editor.commit();
+                }
                 startActivity(intent);
             }
         });
